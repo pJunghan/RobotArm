d9e0262b316e7f99e621fe0610a7c99de11f6b4f
LSM <qqqlsm@gmail.com>
2024-07-05 18:15:30 +0900

    Updated login.ui, signup.ui

--- GUI/main.py
+++ GUI/main.py
@@ -1,27 +1,83 @@
 import sys
+import cv2
+import threading
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt, QEvent, QTimer
 
 # UI 파일 연결
-main_form_class = uic.loadUiType("/home/lsm/git_ws/RobotArm/GUI/main.ui")[0]
-next_form_class = uic.loadUiType("/home/lsm/git_ws/RobotArm/GUI/order_ice_cream.ui")[0]
+main_form_class = uic.loadUiType("/home/lsm/git_ws/RobotArm/GUI/main2.ui")[0]
+login_form_class = uic.loadUiType("/home/lsm/git_ws/RobotArm/GUI/login.ui")[0]
+menu_form_class = uic.loadUiType("/home/lsm/git_ws/RobotArm/GUI/order_ice_cream2.ui")[0]
+
+class VideoThread:
+    def __init__(self, graphics_view):
+        self.graphics_view = graphics_view
+        self.webcam = cv2.VideoCapture(0)
+        if not self.webcam.isOpened():
+            print("Could not open webcam")
+            exit()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(60)  # 30 ms마다 프레임을 갱신합니다.
+
+    def update_frame(self):
+        status, frame = self.webcam.read()
+        if status:
+            # OpenCV의 BGR 이미지를 Qt의 RGB 이미지로 변환합니다.
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            h, w, ch = frame.shape
+            bytes_per_line = ch * w
+            qt_image = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(qt_image)
+            self.display_image(pixmap)
+
+    def display_image(self, pixmap):
+        scene = QGraphicsScene()
+        scene.addItem(QGraphicsPixmapItem(pixmap))
+        self.graphics_view.setScene(scene)
+        self.graphics_view.fitInView(scene.itemsBoundingRect(), 1)  # 1은 AspectRatioMode의 KeepAspectRatio 값입니다.
+
+    def stop(self):
+        self.timer.stop()
+        self.webcam.release()
+
 
 # 메인 창 클래스
 class MainWindow(QMainWindow, main_form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
         self.orderButton.clicked.connect(self.go_to_next_window)
 
     def go_to_next_window(self):
-        self.next_window = NextWindow()
+        self.next_window = LoginWindow()
         self.next_window.show()
         self.close()
 
-# 다음 창 클래스
-class NextWindow(QMainWindow, next_form_class):
+
+class LoginWindow(QMainWindow, login_form_class):
+    def __init__(self):
+        super(LoginWindow, self).__init__()
+        self.setupUi(self)
+        self.video_thread = VideoThread(self.graphicsView)
+        self.orderbtn.clicked.connect(self.go_to_menu_window)
+        self.memberBtn.clicked.connect(self.go_to_menu_window)
+
+
+    def closeEvent(self, event):
+        self.video_thread.stop()
+        event.accept()
+    
+    def go_to_menu_window(self):
+        self.next_window = MenuWindow()
+        self.next_window.show()
+        self.close()
+
+
+# 메뉴 창 클래스
+class MenuWindow(QMainWindow, menu_form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
