2a1dd078a19fc996af7afb48943f43fd3cffae74
LSM <qqqlsm@gmail.com>
2024-07-12 17:26:43 +0900

    Upated Check_Login.ui

---
+++ projectcpy/project (LSM)/face_to_info.py
@@ -0,0 +1,144 @@
+import os
+import cv2
+import numpy as np
+from threading import Thread
+from deepface import DeepFace
+from queue import Queue
+from config import user_img_path
+
+class FaceToInfo():
+    def __init__(self, db_path = user_img_path) :
+        self.db_path = db_path
+        self.cap = cv2.VideoCapture(0)
+        self.db_img = cv2.imread(self.db_path)
+        self.db = os.listdir(self.db_path)
+        self.color = (255, 255, 255) 
+        self.font =  cv2.FONT_HERSHEY_PLAIN
+        self.img_queue = Queue(maxsize=1)
+        
+        self.width = 640
+
+        self.analyze_result = None
+        self.frame = None
+        self.name = None
+
+        self.visualization = False      # 이미지 시각화와 관련된 파라미터 결과값을 보고싶다면 True로
+        self.known_person = False
+        self.log_print_ = False         # 디버깅 메세지들을 확인하기 위한 파라미터 한번에 관리하기 용이
+        self.ret = False
+
+        self.cam_to_info_deamon = True  # 각 스래드를 정지시키기위한 파라미터 False로 할시 해당 스레드 정지
+        self.cam_deamon = True          #
+    
+    def run_cam(self) : # 캠에서 이미지를 받아와 이미지 큐에 넣어주는 함수 결과 시각화가 켜져있다면 해당 결과도 이미지에 추가해줌
+        while self.cam_deamon:
+            self.ret, frame = self.cap.read()
+            self.frame = frame
+            if self.ret:
+                # self.gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                if self.img_queue.full():
+                    self.img_queue.get()
+                
+                self.img_queue.put(frame)
+                if self.analyze_result != None and self.visualization:
+                    self.frame = self.result_visualization(frame=frame)
+                elif self.analyze_result == None and self.visualization:
+                    cv2.putText(frame, "Waiting Service...",  (0, 50),     self.font, 2, (0, 0, 255), 1, cv2.LINE_AA)
+
+    
+            # cv2.imshow("test", frame)
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
+
+        self.cap.release()
+        # cv2.destroyAllWindows()
+        
+    def result_visualization(self, frame) : # 결과값 시각화 함수
+        cv2.putText(frame, self.analyze_result[0]["dominant_gender"],                        (0, 30),     self.font, 2, (0, 255, 0), 1, cv2.LINE_AA)# 성별
+        cv2.putText(frame, self.analyze_result[0]["dominant_emotion"],                       (0, 60),   self.font, 2, (0, 255, 0), 1, cv2.LINE_AA)  # 감정
+        cv2.putText(frame, str(self.analyze_result[0]["age"]),                               (0, 90),   self.font, 2, (0, 255, 0), 1, cv2.LINE_AA)  # 나이
+        
+        cv2.rectangle(frame, (self.face_data["facial_area"]["x"], self.face_data["facial_area"]["y"]), 
+                        (self.face_data["facial_area"]["x"] + self.face_data["facial_area"]["w"], self.face_data["facial_area"]["y"] + self.face_data["facial_area"]["h"]),
+                        color=self.color,
+                        thickness=3) # 얼굴 범위
+        
+        if not self.find_result[0].empty and self.known_person: # db에 존재하는 인물이라면 해당 이미지의 이름
+            self.name = (self.find_result[0]["identity"][0]).split("/")[2].split(".")[0]
+            cv2.putText(frame, self.name, (0, 120),   self.font, 2, (0, 255, 0), 1, cv2.LINE_AA)
+
+        return frame
+
+    def cam_to_info(self) : # 이미지 큐 에서 사람의 얼굴을 가져와서 정보들을 넣어주는 함수
+        while self.cam_to_info_deamon:
+            if self.img_queue.empty(): # 이미지큐가 비어있으면 이번 반복 스킵
+                continue
+
+            frame = self.img_queue.get() # 이미지 큐에서 이미지 가져오기
+            faces = DeepFace.extract_faces(img_path=frame, enforce_detection=False) # 이미지 큐에서 얼굴부분 추출
+            
+            x = 999
+            
+            for face_data in faces: # 다수의 얼굴이 검출될 시 얼굴마다 반복
+                face_img = (face_data["face"] * 255).astype(np.uint8)   # 각 얼굴의 이미지
+                face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)    # 색상 타입 변경
+
+                find_result = DeepFace.find(img_path = face_img, db_path = self.db_path, threshold = 0.8, silent = True, enforce_detection = False) # db에서 0.8의 일치도를 보이는 얼굴이 있는지 확인
+                analyze_result = DeepFace.analyze(img_path = face_img, silent = True, enforce_detection = False)    # 사람의 정보들을 유추
+                
+                if x > abs(face_data["facial_area"]["x"] + face_data["facial_area"]["w"] / 2 - self.width / 2) and face_data["facial_area"]["x"] != 0: # 사람의 얼굴이 가장 중앙에 가까울 시 데이터 저장
+                    x = abs(face_data["facial_area"]["x"] + face_data["facial_area"]["w"] / 2 - self.width / 2) # 다음 비교를 위해 값 저장 
+                    mid_analyze_result = analyze_result
+                    mid_find_result = find_result
+                    mid_face_data = face_data
+                    self.are_you_member(find_result)
+                else:
+                    continue 
+                
+                self.log_print(find_result[0].distance)
+                self.log_print(x)
+
+            if 'mid_analyze_result' in locals(): # 검출 결과 있는지 확인
+                self.analyze_result = mid_analyze_result
+                self.find_result = mid_find_result
+                self.face_data = mid_face_data
+
+    def are_you_member(self, find_result) : # 멤버쉽 멤버이면(db에 인식률이 0.85 이상의 사진이 있다면) 초록색, 아니면 빨간색으로 표시해주는 함수
+        if not find_result[0].empty :
+            if find_result[0].distance.iloc[0] < 0.15:
+                self.color = (0, 255, 0)
+                self.known_person = True
+            else:
+                self.color = (0, 0, 255)
+                self.known_person = False
+        else:
+            self.color = (0, 0, 255)
+            self.known_person = False
+
+    def log_print(self, string) : # self.log_print_ 를 통해 한번에 비활성화, 활성화 가능한 print
+        if self.log_print_ is True:
+            print(string)
+
+    def get_frame(self) : # 이미지 가져올 수 있는 함수
+        if self.frame is None:
+            return False, self.frame
+        else:
+            return True, self.frame
+
+
+    def __del__(self) : 
+        self.cam_to_info_deamon = False  # 각 스래드를 정지시키기위한 파라미터 False로 할시 해당 스레드 정지
+        self.cam_deamon = False          #
+        self.cap.release()
+        # cv2.destroyAllWindows()
+
+
+        
+
+if __name__ == "__main__":
+    face = FaceToInfo()
+    cam_thread = Thread(target=face.run_cam)
+    cam_thread.start()
+    deep_face_thread = Thread(target=face.cam_to_info)
+    deep_face_thread.start()
---
+++ projectcpy/project (LSM)/login_window.py
@@ -0,0 +1,132 @@
+import sys
+import cv2
+import pymysql
+import time
+from PyQt5 import uic, QtCore
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QPixmap, QImage, QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QGraphicsScene
+from face_to_info import FaceToInfo
+from menu_window import MenuWindow
+from threading import Thread
+from new_account_window import NewAccountWindow
+from config import login_ui_path, db_config, new_account_ui_path, user_img_path
+
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super(LoginWindow, self).__init__()
+        uic.loadUi(login_ui_path, self)
+
+        self.face = FaceToInfo(user_img_path)
+        cam_thread = Thread(target=self.face.run_cam)
+        cam_thread.start()
+        deep_face_thread = Thread(target=self.face.cam_to_info)
+        deep_face_thread.start()
+        self.face.visualization = True
+
+        # self.camera = cv2.VideoCapture(0)  # Open the default camera (0)
+        # if not self.camera.isOpened():
+        #     QMessageBox.critical(self, "카메라 연결 오류", "카메라를 열 수 없습니다.")
+        #     return
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(1000 // 30)  # Update frame every 33 milliseconds (30 fps)
+
+        self.orderbtn.clicked.connect(self.handle_guest_login)
+        self.memberBtn.clicked.connect(self.go_to_new_account_window)
+
+        self.scene = QGraphicsScene(self)
+        self.graphicsView.setScene(self.scene)
+
+    def update_frame(self):
+        ret, frame = self.face.get_frame()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            h, w, ch = frame.shape
+            bytesPerLine = ch * w
+            convertToQtFormat = QImage(frame.data, w, h, bytesPerLine, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(convertToQtFormat)
+            
+            # Clear the previous items in the scene
+            self.scene.clear()
+            
+            # Add the new pixmap item to the scene
+            pixmap_item = self.scene.addPixmap(pixmap)
+            
+            # Scale the pixmap item to fit the graphics view
+            pixmap_item.setScale(1)
+            
+            # Ensure the pixmap item is centered in the graphics view
+            pixmap_item.setPos((self.graphicsView.width() - pixmap.width()) / 2,
+                               (self.graphicsView.height() - pixmap.height()) / 2)
+        self.check_user()
+        
+    def check_user(self):
+        if self.face.known_person and not self.isHidden():
+            self.go_to_menu_window()
+
+
+    def close_event(self):
+        self.timer.stop()
+        self.face.cam_to_info_deamon = False
+        self.face.cam_deamon = False
+        del(self.face)
+        self.close()
+        
+
+    def handle_guest_login(self):
+        # 데이터베이스에 새로운 비회원 사용자 추가
+        guest_name = self.create_guest_user()
+        if guest_name:
+            self.go_to_menu_window()
+
+    def create_guest_user(self):
+        try:
+            conn = pymysql.connect(**db_config)
+            with conn.cursor() as cursor:
+                # Find the smallest unused user_ID
+                cursor.execute("SELECT user_ID FROM user_info_table ORDER BY user_ID")
+                existing_ids = {row['user_ID'] for row in cursor.fetchall()}
+
+                new_user_id = 1
+                while new_user_id in existing_ids:
+                    new_user_id += 1
+
+                # Generate new undefined guest name
+                new_guest_name = f"undefined_{new_user_id}"
+
+                # Insert new user record
+                insert_query = """
+                INSERT INTO user_info_table (user_ID, name, point)
+                VALUES (%s, %s, %s)
+                """
+                cursor.execute(insert_query, (new_user_id, new_guest_name, 0))
+                conn.commit()
+
+                return new_guest_name
+
+        except pymysql.MySQLError as err:
+            print(f"데이터베이스 오류 발생: {err}")
+            QMessageBox.warning(self, "오류", f"데이터베이스 오류 발생: {err}")
+            return None
+
+        finally:
+            if 'conn' in locals():
+                conn.close()
+                print("데이터베이스 연결을 닫았습니다.")
+
+
+    def go_to_menu_window(self):
+        self.close_event()
+        # self.hide()  # 메인 윈도우를 숨깁니다.
+        self.next_window = MenuWindow(db_config)
+        self.next_window.show()
+
+
+
+    def go_to_new_account_window(self):
+        self.close_event()
+        # self.hide()  # 메인 윈도우를 숨깁니다.
+        self.next_window = NewAccountWindow(new_account_ui_path, db_config)
+        self.next_window.show()
