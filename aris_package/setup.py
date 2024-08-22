59181cec883ebfea730e102dcadd869eeb5a6f90
pJunghan <jh051099@naver.com>
2024-08-22 09:28:21 +0900

    add aris_package

---
+++ aris_package/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+import glob
+
+package_name = 'aris_package'
+
+setup(
+    name=package_name,
+    version='0.1.0',  # 적절한 버전 번호로 업데이트
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),  # 패키지 인덱스에 마커 파일 설치
+        ('share/' + package_name, ['package.xml']),  # package.xml 파일을 설치
+        ('lib/' + package_name, glob.glob('launch/*.launch.py')),  # launch 파일이 있다면 설치
+        ('share/' + package_name, glob.glob('config/*.yaml')),  # 설정 파일이 있다면 설치
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rds',
+    maintainer_email='parkjh05109@gmail.com',
+    description='ROS2 package for robotic arm and delivery system',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'motion2 = aris_package.motion2:main',
+        ],
+    },
+)
