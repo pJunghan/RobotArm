6d74c5e2cbc52aefb480239776db3ccc37c3149e
rds <wogur21c@gmail.com>
2024-08-22 14:40:42 +0900

    check point

---
+++ aris_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'aris_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rds',
+    maintainer_email='wogur21c@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'aris = aris_pkg.motion3:main'
+            
+        ],
+    }
+)
